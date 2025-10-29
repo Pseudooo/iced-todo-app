755e46848f1f044659f561f972c9b3cbee665653
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-29 23:39:08 +0000

    wip drawer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use crate::drawer::drawer;
 use crate::new_task::{NewTaskMessage, NewTaskState};
-use crate::Message::NullMessage;
-use iced::widget::{button, column, row};
-use iced::{Element, Fill, Theme};
+use crate::Message::{NullMessage, ToggleNewTaskForm, WindowResized};
+use iced::widget::{button, container, row};
+use iced::{Element, Fill, Size, Subscription, Theme};
 
 mod new_task;
+mod drawer;
 
 fn main() -> iced::Result {
     iced::application("Todo App!", TodoAppState::update, TodoAppState::view)
@@ -14,34 +16,40 @@ fn main() -> iced::Result {
 
 #[derive(Default)]
 pub struct TodoAppState {
+    pub window_size: Size,
     pub show_new_task_form: bool,
     pub new_task_state: NewTaskState,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    ToggleNewTaskForm(bool),
+    ToggleNewTaskForm,
     NewTaskMessage(NewTaskMessage),
+    WindowResized(Size),
     NullMessage,
 }
 
 impl TodoAppState {
     pub fn view(&self) -> Element<'_, Message> {
-
         let controls = Self::get_controls();
+        let container = container(controls)
+            .width(Fill)
+            .height(Fill);
 
-        if self.show_new_task_form {
-            column![controls, self.new_task_state.view().map(map_new_task_message)]
-                .into()
-        } else {
-            controls
-        }
+        drawer(
+            self.show_new_task_form,
+            ToggleNewTaskForm,
+            || self.new_task_state.view().map(map_new_task_message),
+            self.window_size,
+            container,
+            )
+            .into()
     }
 
     fn get_controls<'a>() -> Element<'a, Message> {
         let new_task_button = button("New Task")
             .style(button::success)
-            .on_press(Message::ToggleNewTaskForm(true));
+            .on_press(ToggleNewTaskForm);
 
         row![new_task_button]
             .padding(5)
@@ -51,21 +59,33 @@ impl TodoAppState {
 
     pub fn update(&mut self, message: Message) {
         match message {
-            Message::ToggleNewTaskForm(show_new_task_form) => {
-                self.show_new_task_form = show_new_task_form;
+            Message::ToggleNewTaskForm => {
+                self.show_new_task_form = !self.show_new_task_form;
             }
             Message::NewTaskMessage(message) => {
                 self.new_task_state.update(message)
-            }
+            },
+            WindowResized(size) => {
+                self.window_size = size;
+            },
             NullMessage => println!("Null Message"),
             _ => println!("Unknown message")
         }
+    }
+
+    pub fn subscription(&self) -> Subscription<Message> {
+        iced::event::listen_with(|e, _, _| match e {
+            iced::Event::Window(iced::window::Event::Resized(size)) => {
+                Some(WindowResized(size))
+            }
+            _ => None,
+        })
     }
 }
 
 fn map_new_task_message(message: NewTaskMessage) -> Message {
     match message {
-        NewTaskMessage::CancelNewTask => Message::ToggleNewTaskForm(false),
+        NewTaskMessage::CancelNewTask => Message::ToggleNewTaskForm,
         _ => Message::NewTaskMessage(message),
     }
 }
