a4e133dd1fcabaee3e5c7ae21aad49e111355490
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-30 13:15:16 +0000

    Added task item

---
+++ src/task.rs
@@ -0,0 +1,49 @@
+use crate::task::TaskMessage::ToggleCompleted;
+use iced::widget::{checkbox, column, container, row, text};
+use iced::{Center, Element, Fill};
+use iced::advanced::graphics::text::cosmic_text::Align;
+use iced_aw::date_picker::Date;
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+pub struct TaskState {
+    pub id: Uuid,
+    pub completed: bool,
+    pub title: String,
+    pub description: Option<String>,
+    pub due_date: Option<Date>,
+}
+
+#[derive(Debug, Clone)]
+pub enum TaskMessage {
+    ToggleCompleted(bool),
+}
+
+impl TaskState {
+    pub fn view(&self) -> Element<'_, TaskMessage> {
+        let completed_toggle = checkbox("", self.completed)
+            .on_toggle(ToggleCompleted);
+
+        let title_label = text(self.title.as_str());
+        let description_label = text(self.description.clone().unwrap_or("".to_string()));
+        let details_column = column![
+            title_label,
+            description_label,
+        ];
+
+        row![
+            completed_toggle,
+            details_column,
+            ]
+            .align_y(Center)
+            .into()
+    }
+
+    pub fn update(&mut self, message: TaskMessage) {
+        match message {
+            ToggleCompleted(state) => {
+                self.completed = state;
+            }
+        }
+    }
+}
