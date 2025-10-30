249e9a0dcf11926ad8b6dde720efd13189b35750
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-30 23:24:26 +0000

    Prevented drawer overlapping the scrollbar

--- src/drawer.rs
+++ src/drawer.rs
@@ -14,14 +14,9 @@ where Message: Clone + 'a
     const PADDING: f32 = 5.0;
     const MAX_HEIGHT: f32 = 350.0;
 
-    let height = match is_open {
-        true => MAX_HEIGHT,
-        false => 0.0,
-    };
-
-    let background = match is_open {
-        true => Color::from_rgba(0.0, 0.0, 0.0, 0.75),
-        false => Color::TRANSPARENT,
+    let (height, background) = match is_open {
+        true => (MAX_HEIGHT, Color::from_rgba(0.0, 0.0, 0.0, 0.75)),
+        false => (0.0, Color::TRANSPARENT),
     };
 
     let motion = Motion::SNAPPY;
@@ -51,6 +46,11 @@ where Message: Clone + 'a
         .push(
             // Drawer content
             animation_builder((background, height), move |(background, height)| {
+                // Hacky way to prevent the drawer being visible when its closed
+               if height <= 0.0 {
+                   return Space::new(Fill, Fill).into();
+               }
+
                 let offset_y = height - MAX_HEIGHT + PADDING * height / MAX_HEIGHT;
                 Offset::new(
                     container(
